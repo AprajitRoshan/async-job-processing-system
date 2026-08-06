e372f99c795f6191e4b0317178dc8e12e276f900
rosha <aprajit.roshan@gmail.com>
2026-08-07 05:18:45 +0530

    main

--- app/main.py
+++ app/main.py
@@ -8,17 +8,21 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Allow React frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+
+        # Netlify
+        "https://async-job-processing-system.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(job_router)
 
 
